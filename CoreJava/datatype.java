96a040d211d675c34b160e25f651fdd0416febff
JancyDes <jancydes12@gmail.com>
2026-06-03 14:24:03 +0530

    Added datatype.java

---
+++ CoreJava/datatype.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class datatype
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        Integer a = 12;
+        float b = 3.01f;
+        double c = 20.00020229;
+        boolean d = true;
+        System.out.println("Integer : "+a);
+        System.out.println("Float : "+b);
+        System.out.println("double : "+c);
+        System.out.println("boolean : "+d);
+    }
+}
